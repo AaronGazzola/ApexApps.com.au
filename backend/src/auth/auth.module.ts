8e36be0e6524163ebb8931aef135996349e0496c
Aaron Gazzola <aaron@apexapps.dev>
2021-07-27 20:32:07 +1000

    basic user auth set up

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import {UserSchema} from './schemas/user.schema'
+import { JwtStrategy } from './strategies/jwt-auth.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema}]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d'}
+    })
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
 export class AuthModule {}
