3bd00d923113f3fad99e596c42350c3b81f37bce
Aaron Gazzola <aaron@apexapps.dev>
2021-07-27 18:20:00 +1000

    nest rebuild, mongo connect with URI string in .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import config from './config/keys'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(config.mongoURI, {useNewUrlParser: true})
-  ],
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_DEV, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }),
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
