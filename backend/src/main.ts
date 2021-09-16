610bd54e7bfcd6b825c4c3baa60a28c63bbe4dec
Aaron Gazzola <aaron@apexapps.com.au>
2021-09-16 19:40:30 +1000

    cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(
+    process.env.NODE_ENV === 'development'
+      ? { origin: 'http://localhost:3000' }
+      : { origin: 'https://apexapps.com.au' },
+  );
   await app.listen(5000);
 }
 bootstrap();
